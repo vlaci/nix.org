0d907ce2faa2eab53790551640f98532f9ae6120
László Vaskó <1771332+vlaci@users.noreply.github.com>
2024-11-27 22:45:59 +0100

    sidenotes: add to layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,13 +31,14 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.Graph(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.DesktopOnly(Component.Sidenotes()),
   ],
+  afterBody: [Component.Backlinks()],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
