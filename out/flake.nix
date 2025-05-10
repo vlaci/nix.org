f8ee1c5ea1d3074b2930836e52b1704023d0c5f7
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-05-10 13:06:10 +0200

    emacs: do-not rebuild init configuration when flake reference changes

--- out/flake.nix
+++ out/flake.nix
@@ -2305,7 +2305,7 @@
                                 major = removeLeadingZeros (builtins.substring 0 8 ver);
                                 minor = removeLeadingZeros (builtins.substring 8 6 ver);
                               in
-                              "${major}.${minor}";
+                              args.version or "${major}.${minor}";
                           in
                           final.melpaBuild (
                             {
@@ -2367,6 +2367,7 @@
                         gcmh
                         (mkPackage {
                           pname = "vlaci-emacs";
+                          version = "1.0";
                           src = pkgs.writeText "vlaci-emacs.el" ''
                             ;;; vlaci-emacs.el --- local extensions -*- lexical-binding: t; -*-
 
