86c9240656b22c21cfd68f346c3f4f984c32cfe5
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-06-19 08:43:03 +0200

    pinentryPackage -> pinentry.package

--- out/flake.nix
+++ out/flake.nix
@@ -2039,7 +2039,7 @@
                     services.gpg-agent = {
                       enable = true;
                       enableSshSupport = true;
-                      pinentryPackage = pkgs.pinentry-gnome3;
+                      pinentry.package = pkgs.pinentry-gnome3;
                     };
                   }
                 )
