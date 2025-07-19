bbd66cc3a522c218963669c1bf402ced4f489cd5
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-07-19 15:04:32 +0200

    ente: add package

--- out/flake.nix
+++ out/flake.nix
@@ -2154,6 +2154,12 @@
                 }
                 (
                   { pkgs, ... }:
+                  {
+                    home.packages = with pkgs; [ ente-desktop ];
+                  }
+                )
+                (
+                  { pkgs, ... }:
 
                   {
                     home.packages = [ pkgs.vlaci-emacs ];
